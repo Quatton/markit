29df4e0b915e6cb686d9b74fd05efe9e3e03da65
Quatton <atomicatives@gmail.com>
2023-07-10 04:26:00 +0900

    template: better header

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="sticky top-0 z-40 border-b bg-background/80 backdrop-blur-sm">
-    <div class="container flex h-16 items-center px-4">
+    <div class="flex h-16 items-center px-4">
       <Brand />
       <div class="ml-auto">
         <LayoutMainNav />
